9a84b28915da92d099077e5c1ec2af3869ff3add
Alexey Yurko <a.yurko@gmail.com>
2023-08-11 20:07:47 +0300

    Fix for empty PDF metadata
    - handling of PDF metadata extraction to prevent failure when metadata is not provided

--- importers/pdf.py
+++ importers/pdf.py
@@ -13,9 +13,18 @@ from importers.base import (
 class PdfImporter(BookImporter):
     FORMAT = 'pdf'
 
+    def _read_pdf_file(self, reader_type):
+        if reader_type == 'pdfium':
+            return pdfium.PdfDocument(self._get_file_attribute())
+        elif reader_type == 'pypdf':
+            return PdfFileReader(self._get_file_attribute())
+
+    def _get_file_attribute(self):
+        return self.file.file._file
+
     def extract_cover(self):
         filename = self._cover_filename
-        pdf = pdfium.PdfDocument(self.file.file._file)
+        pdf = self._read_pdf_file('pdfium')
         page = pdf.get_page(0)
         cover = page.render_topil(scale=2, optimise_mode=pdfium.OptimiseMode.NONE)
         path = os.path.join(IMAGES_PATH, filename)
@@ -24,11 +33,16 @@ class PdfImporter(BookImporter):
         return filename
 
     def get_metadata(self):
-        pdf = PdfFileReader(self.file.file)
+        pdf = self._read_pdf_file('pypdf')
         pdf_info = pdf.metadata
-        description = pdf_info.get('/Description', pdf_info.get('/Subject')) or ''
-        authors = str(pdf_info.get('/Author', '')).split(',')
-        title = pdf_info.get('/Title') or os.path.splitext(self.file.filename)[0]
+        if pdf_info:
+            description = pdf_info.get('/Description', pdf_info.get('/Subject')) or ''
+            authors = str(pdf_info.get('/Author', '')).split(',')
+            title = pdf_info.get('/Title') or self.file.filename.split('.')[0]
+        else:
+            description = ''
+            authors = ''
+            title = self.file.filename.split('.')[0]
         return BookMetadata(
             authors=authors,
             title=title,
