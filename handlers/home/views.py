3d41a13dc25e38b84e6df0a434846ca9fb0ae02a
Alexey Yurko <a.yurko@gmail.com>
2024-09-30 19:04:47 +0300

    Do not show tags without books
    - books list - skip tags not linked to books

--- handlers/home/views.py
+++ handlers/home/views.py
@@ -3,11 +3,12 @@ from fastapi import (
     Depends,
     Request,
 )
-from sqlalchemy import select
-from sqlalchemy.orm import Session
 from starlette.templating import Jinja2Templates
 
-from db import get_db_session
+from handlers.home.dependencies import (
+    get_all_books,
+    get_tags_linked_to_books,
+)
 from models import (
     Book,
     Tag,
@@ -19,7 +20,9 @@ templates = Jinja2Templates(directory="templates")
 
 
 @router.get("/")
-def homepage(request: Request, db_session: Session = Depends(get_db_session)):
-    books = db_session.scalars(select(Book)).all()
-    tags = db_session.scalars(select(Tag).order_by(Tag.name)).all()
+def homepage(
+        request: Request,
+        books: list[Book] = Depends(get_all_books),
+        tags: list[Tag] = Depends(get_tags_linked_to_books),
+):
     return templates.TemplateResponse('index.html', {'request': request, 'books': books, 'tags': tags})
